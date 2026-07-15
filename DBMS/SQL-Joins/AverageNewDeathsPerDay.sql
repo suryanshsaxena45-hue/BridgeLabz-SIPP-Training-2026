14e2a8037bbc7c1ff5376f71ef7879dd698fc012
Suryansh Saxena <suryanshsaxena45@gmail.com>
2026-07-15 08:59:21 +0530

    Delete DBMS directory

--- DBMS/SQL-Joins/AverageNewDeathsPerDay.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    record_date, 
-    AVG(new_deaths) AS avg_daily_deaths
-FROM 
-    covid_deaths
-GROUP BY 
-    record_date
-ORDER BY 
-    record_date ASC;
--- DBMS/SQL-Joins/DeathsAndVaccinations.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    cd.country_name, 
-    cd.total_deaths, 
-    cv.vaccination_status,
-    cv.total_vaccinated
-FROM 
-    covid_deaths cd
-LEFT JOIN 
-    covid_vaccines cv ON cd.country_id = cv.country_id;
