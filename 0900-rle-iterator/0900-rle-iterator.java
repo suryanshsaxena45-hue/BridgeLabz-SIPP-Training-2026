d15aecc190d4ed27199102e594b15e5e475ceb2e
Suryansh Saxena <suryanshsaxena45@gmail.com>
2026-07-15 08:50:10 +0530

    Time: 5 ms (37.4%), Space: 44.1 MB (47.92%) - LeetHub

--- 0900-rle-iterator/0900-rle-iterator.java
+++ 0900-rle-iterator/0900-rle-iterator.java
@@ -8,19 +8,19 @@ class RLEIterator {
     }
     
     public int next(int n) {
-        // Walk through the encoded array while there are elements left to process
+        
         while (index < encoding.length) {
             if (n > encoding[index]) {
-                // Exhaust the entire current run and subtract its capacity from n
+                
                 n -= encoding[index];
                 index += 2; 
             } else {
-                // Deduct remaining n from the current run and return its value
+                
                 encoding[index] -= n;
                 return encoding[index + 1];
             }
         }
-        // Return -1 if all elements are exhausted before completing the step
+        
         return -1;
     }
 }
